7d421be8031c4c5e3aae932278c8d4ad165b3a72
Peter-Paul <ppmunga@gmail.com>
2025-11-18 21:57:17 +0300

    Fix: Added meta data function

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,28 @@ export const metadata: Metadata = {
   description: "Your one-stop shop for all things botnite",
 };
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    other: {
+      "fc:miniapp": JSON.stringify({
+        version: "next",
+        imageUrl:
+          "https://res.cloudinary.com/dwf6iuvbh/image/upload/v1761406429/Gemini_Generated_Image_roqu2wroqu2wroqu-removebg-preview_ktxto3.png",
+        button: {
+          title: `Open Bontnite Store`,
+          action: {
+            type: "launch_miniapp",
+            name: "Botnite Store",
+            url: "https://your-app.com",
+            splashImageUrl: "https://your-app.com/splash-image",
+            splashBackgroundColor: "#000000",
+          },
+        },
+      }),
+    },
+  };
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
